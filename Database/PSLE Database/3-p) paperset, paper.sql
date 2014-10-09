092ee99d99a4d919b6d3174efab8d7be14a217cc
Ly <cymn_td@yahoo.com>
2014-10-09 21:30:57 +0800

    Documentation

---
+++ Database/PSLE Database/3-p) paperset, paper.sql
@@ -0,0 +1,39 @@
+INSERT INTO `DBManagement_paperset` VALUES
+(1+100,'St Hilda Primary School SA2',4),
+(2+100,'Nan Hua Primary School SA2',4),
+(3+100,'Nanyang Primary School SA2',4),
+(4+100,'Tao Nan Primary School SA2',4),
+(5+100,'Anglo Chinese School SA2',4),
+(6+100,'Raffles Girls Primary School SA2',4),
+(7+100,'Christian Brothers School SA2',4),
+(8+100,'Singapore Hokkien Huay Kwan SA2',4),
+(9+100,'2008 PSLE Mathematics',4),
+(10+100,'2013 Singapore Chinese Girls School SA2',4),
+(11+100,'2013 Tao Nan Primary School SA2',4),
+(12+100,'2013 Rosyth School SA2',4),
+(13+100,'2013 Raffles Girls School SA2',4),
+(14+100,'2013 Nanyang Primary School SA2',4),
+(15+100,'2013 Nan Hua Primary School SA2',4),
+(16+100,'2013 Singapore Hokkien Huay Kwan SA2',4),
+(17+100,'2013 Methodist Girls Primary School SA2',4),
+(18+100,'2013 CHIJ St Nicholas Primary School SA2',4);
+
+
+INSERT INTO `DBManagement_paper` VALUES 
+('201201004','2012','St Hilda Primary SA2',1,4,1+100),	#2012011201
+('201202004','2012','St Hilda Primary SA2',2,4,1+100),	#2012011202
+('201203004','2012','Nan Hua Primary SA2',1,4,2+100),	#2012021201
+('201204004','2012','Nan Hua Primary SA2',2,4,2+100),	#2012021202
+('201205004','2012','Nanyang Primary SA2',1,4,3+100),	#2012031201
+('201206004','2012','Nanyang Primary SA2',2,4,3+100),	#2012031202
+('201207004','2012','Tao Nan Primary SA2',1,4,4+100),	#2012041201
+('201208004','2012','Tao Nan Primary SA2',2,4,4+100),	#2012041202
+('201209004','2012','Anglo Chinese School SA2',1,4,5+100),		#2012051201
+('201210004','2012','Anglo Chinese School SA2',2,4,5+100),	#2012051202
+('201211004','2012','Raffles Girls Primary SA2',1,4,6+100),	#2012061201
+('201212004','2012','Raffles Girls Primary SA2',2,4,6+100),	#2012061202
+('201213004','2012','Christian Brothers School SA2',1,4,7+100),	#2012071201
+('201214004','2012','Christian Brothers School SA2',2,4,7+100),	#2012071202
+('201215004','2012','Singapore Hokkien Huay Kwan SA2',1,4,8+100),	#2012081201
+('201216004','2012','Singapore Hokkien Huay Kwan SA2',2,4,8+100);	#2012081202
+
